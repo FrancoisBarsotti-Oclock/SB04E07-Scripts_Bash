9560193d500eb29819d59f63dc14d925f6f7aac5
François Barsotti <franc_site@yahoo.com>
2026-02-03 22:20:29 +0100

    mise en forme de l'exercice avec l'argument

--- Exo2.sh
+++ Exo2.sh
@@ -4,18 +4,20 @@
 # dans le nom du fichier de sauvegarde.
 
 #!/bin/bash
-if (( $# != 1 ))
+if (( $# != 1 )) # on établit le nombre d'arguments, (dans ce cas ce n'est qu'un seul.
 then
     echo "1 argument requis : $0 <repertoire_a_sauvegarder>"
     exit 1
 fi
 
-SOURCE=$1
+SOURCE=$1 # Argument donné lors de la première exécution d'Exo2 (avec route du fichier le contenant)
 
-if [ ! -d $SOURCE ]
+if [ ! -d $SOURCE ] #Vérification de l'existance du dossier
 then
     echo "Le répertoire n'existe pas"
     exit 1
 fi
 
-DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+DATE=$(date +"%Y-%m-%d_%H-%M-%S") # Récupération de la date actuelle, selon format décidé par nous
+
+echo $DATE
